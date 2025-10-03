ccc0c385668d25f373be20d28fa673a732de65e4
oritise1 <ikareemabiodun730@gmail.com>
2025-10-04 00:08:15 +0100

    edited svg icon

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -61,7 +61,7 @@ const Socials = ({ isRegisterPage = false }) => {
           </span>
         ) : (
           <>
-            <img src={GitHubLogo} alt="github logo" className="w-6 h-6" />
+            <img src={GitHubLogo} alt="github logo" className="" />
             <span className="font-medium text-xs sm:text-sm">
               {!isRegisterPage && "Continue with"} Github
             </span>
@@ -99,7 +99,7 @@ const Socials = ({ isRegisterPage = false }) => {
           </span>
         ) : (
           <>
-            <img src={googleLogo} alt="google logo" className="w-5 h-5" />
+            <img src={googleLogo} alt="google logo" className="" />
             <span className="font-medium text-xs sm:text-sm">
               {!isRegisterPage && "Continue with"} Google
             </span>
