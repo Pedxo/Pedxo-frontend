a54c9dde4f50051e01528ff525a18afd42b044e5
oritise1 <ikareemabiodun730@gmail.com>
2026-01-23 11:57:05 +0100

    feat: Update Overview component to display a placeholder for total expenses

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -252,8 +252,11 @@ const Overview = () => {
               <div className="flex justify-between bg-white border rounded-2xl py-3 px-[21px] xl:py-10 xl:px-16">
                 <div className="flex items-center gap-4">
                   <img src={moneybag} alt="" />
-                  <span className="text-2xl font-semibold xl:text-[40px] overview-text">
+                  {/* <span className="text-2xl font-semibold xl:text-[40px] overview-text">
                     {formatCurrency(displayTotalExpenses, currencyCode, locale)}
+                  </span> */}
+                  <span className="text-2xl font-semibold xl:text-[40px] overview-text">
+                    $00.00
                   </span>
                 </div>
               </div>
