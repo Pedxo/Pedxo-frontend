21331bc028cbf39519a7923fbcc8cd842a33bfed
Michael Jonathan <michaelp4great@gmail.com>
2026-05-27 09:32:15 +0100

    contract agreement page fixed

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,27 @@
 import { NavLink } from "react-router-dom";
+import NoDataImg from "../assets/png/no_data.png";
+
 
 function PageNotFound() {
   return (
     <section className="w-full h-screen">
       <div className="flex flex-col items-center justify-center gap-5 h-screen text-center mx-[21px] xl:mx-[123px] xl:text-left">
-        <div className="text-[30px] font-bold  xl:text-[80px]">Oops!</div>
+      <div className="flex items-center justify-center w-full">
+          <img src={NoDataImg} 
+          alt="Page not found"
+          className="
+          w-[220px]
+          sm:w-[280px]
+          md:w-[340px]
+          lg:w-[400px]
+          xl:w-[460px]
+          h-auto
+          object-contain
+          drop-shadow-lg
+          animate-fadeIn
+        "
+          h-auto/>
+        </div>
         <p className="text-2xl text-center xl:text-[40px] font-medium leading-normal max-w-[450px]">
           We can&apos;t find the page you are looking for
         </p>
